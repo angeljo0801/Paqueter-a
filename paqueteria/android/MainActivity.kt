e245ce62e846b27e62a8afabc982c8b2470a5a63
angeljo0801 <angeljo0801@gmail.com>
2026-09-21 18:29:03 -0400

    Paqueteria Android: register backup bridge

---
+++ paqueteria/android/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.angelapps.paqueteria
+
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+
+class MainActivity : FlutterActivity() {
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+        PaqueteriaBackupStorageBridge.register(
+            applicationContext,
+            flutterEngine.dartExecutor.binaryMessenger
+        )
+    }
+}
